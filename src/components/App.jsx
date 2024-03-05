66a46a2790aae0fae801ccd5336b1fec07eef2f5
shouma237 <jonatha.mariano15@gmail.com>
2024-03-05 14:26:56 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,52 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export const App = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  const [contacts, setContacts] = useState(
+    savedContacts !== null ? JSON.parse(savedContacts) : initialContacts
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
+    setContacts(prevState => [...prevState, newContact]);
   };
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-
-    if (savedContacts !== null) {
-      // If something has already been saved in localStorage, we write THIS in the state
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
-
-  componentDidUpdate(_prevProps, prevState) {
-    const { contacts } = this.state;
-
-    if (contacts !== prevState.contacts) {
-      // if a contact is new, set value to localStorage
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  addContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  setFilter = filterValue => {
-    this.setState({
-      filter: filterValue,
-    });
-  };
-
-  filterContact = () => {
-    const { contacts, filter } = this.state;
+  const filterContact = () => {
     const filterLowerCase = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filterLowerCase)
     );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} contacts={contacts} />
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} contacts={contacts} />
 
-        <h2>Contacts</h2>
-        <Filter filter={filter} setFilter={this.setFilter} />
-        <ContactList
-          filterContact={this.filterContact}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList
+        filterContact={filterContact}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
+};
